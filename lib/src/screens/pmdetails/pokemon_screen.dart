7b370352f061ebc4296c8862afbcefa9b6d1474d
Adrian Renner <a.renner+git@eosn.de>
2019-03-30 14:22:19 +0100

    setup BlocProviderTree in pokemon screen

--- lib/src/screens/pmdetails/pokemon_screen.dart
+++ lib/src/screens/pmdetails/pokemon_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/src/screens/pmdetails/pokemon_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex/src/screens/pmdetails/pokemon_state.dart';
 
 class PokemonScreen extends StatefulWidget {
   @override
@@ -11,28 +13,50 @@ class _PokemonScreenState extends State<PokemonScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Pokemon Details')),
-      body: Text('A Pokemon'),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.refresh),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.menu,
-                color: Theme.of(context).accentColor,
-              ),
-              onPressed: () => Navigator.of(context).pushNamed('/count'),
+    return BlocProviderTree(
+      blocProviders: [BlocProvider<PokemonBloc>(bloc: _pokemonBloc)],
+      child: PokemonDetails(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pokemonBloc.dispose();
+    super.dispose();
+  }
+}
+
+class PokemonDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final PokemonBloc _pokemonBloc = BlocProvider.of<PokemonBloc>(context);
+    return BlocBuilder(
+      bloc: _pokemonBloc,
+      builder: (_, PokemonState state) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Pokemon Details')),
+          body: Text('${state.id}'),
+          floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            child: Icon(Icons.refresh),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.menu,
+                    color: Theme.of(context).accentColor,
+                  ),
+                  onPressed: () => Navigator.of(context).pushNamed('/count'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
