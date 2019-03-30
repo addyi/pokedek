8212e2d3e915b6f0fed07a504d1a2a258f2c5b5b
Adrian Renner <a.renner+git@eosn.de>
2019-03-30 14:42:27 +0100

    register StateTransitionSupervisor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedex/src/blocs/transaction_supervisor.dart';
 import 'src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  BlocSupervisor().delegate = SimpleStateTransitionSupervisor();
+  runApp(App());
+}
