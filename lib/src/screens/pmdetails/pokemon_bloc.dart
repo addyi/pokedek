45bfe95f47ea6913001024fa15edf0f527acfbe3
Adrian Renner <a.renner+git@eosn.de>
2019-04-03 11:33:47 +0200

    add loading and error state in mapEventToState

--- lib/src/screens/pmdetails/pokemon_bloc.dart
+++ lib/src/screens/pmdetails/pokemon_bloc.dart
@@ -10,15 +10,24 @@ class PokemonBloc extends Bloc<PokemonEvent, PokemonState> {
   @override
   PokemonState get initialState => PokemonEmpty();
 
-  var _random = Random();
+  final Random _random = Random();
 
   @override
   Stream<PokemonState> mapEventToState(PokemonEvent event) async* {
     if (event is FetchPokemonEvent) {
+      yield PokemonLoading();
+      yield await _fetchPokemon();
+    }
+  }
+
+  Future<PokemonState> _fetchPokemon() async {
+    try {
       int rand = _random.nextInt(806) + 1; // min 1, max 807
       var result = await get('https://pokeapi.co/api/v2/pokemon/$rand/');
       var decodedJson = jsonDecode(result.body);
-      yield PokemonLoaded.newPokemon(decodedJson['id'], decodedJson);
+      return PokemonLoaded.newPokemon(decodedJson['id'], decodedJson);
+    } catch (_) {
+      return PokemonError();
     }
   }
 
